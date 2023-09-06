5a3ace85dba2b028053bd05488a69922a633cdc1
Lucas Gutierrez <lucasguti.98@gmail.com>
2023-09-06 11:08:02 -0300

    Cambio de ruta y agregue carpetas

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 #Enpoint 1
 @app.get("/userdata/{User_id}")
 def userdata(User_id : str):
-    df_endpoint1 = pd.read_csv("df_endpoint1.csv")
+    df_endpoint1 = pd.read_csv("datos\df_endpoint1.csv")
     df_endpoint1['user_id'] = df_endpoint1['user_id'].astype(str)
     user_data = df_endpoint1[df_endpoint1["user_id"] == User_id]
     user_data_dict = user_data.to_dict(orient="records")
@@ -20,7 +20,7 @@ def userdata(User_id : str):
 #Enpoint 3
 @app.get("/genre_rank/{genre}")
 def genre(genre : str): 
-    df_endpoint3 = pd.read_csv("df_endpoint3.csv")
+    df_endpoint3 = pd.read_csv("datos\df_endpoint3.csv")
     rank = df_endpoint3[df_endpoint3['genre'] == genre]
     genre_rank = rank.to_dict(orient="records")
     
@@ -32,7 +32,7 @@ def genre(genre : str):
 #Endpoint 5
 @app.get("/developer_data/{developer}")
 def developer(developer : str):
-    df_endpoint5 = pd.read_csv("df_endpoint5.csv")
+    df_endpoint5 = pd.read_csv("datos\df_endpoint5.csv")
     developer_data = df_endpoint5[df_endpoint5["developer"] == developer]
     developer_data_dict = developer_data.to_dict(orient="records")
 
@@ -42,7 +42,7 @@ def developer(developer : str):
 #Enpoint 6
 @app.get("/sentiment_per_year/{year}")
 def sentiment_analysis(year : int):
-    df_endpoint6 = pd.read_csv("df_endpoint6.csv")
+    df_endpoint6 = pd.read_csv("datos\df_endpoint6.csv")
     sentiment_data = df_endpoint6[df_endpoint6["Year"] == year]
     sentiment_data_dict = sentiment_data.to_dict(orient="records")
 
@@ -52,7 +52,7 @@ def sentiment_analysis(year : int):
 #Endpoint Recommendation system
 @app.get("/Recommendation_system/{Game}")
 def recomendacion_juego(Game : str):   
-    df_RS = pd.read_csv("Recommendation_system.csv")
+    df_RS = pd.read_csv("datos\Recommendation_system.csv")
     RS_data = df_RS[df_RS["Game"] == Game]
     RS_data_dict = RS_data.to_dict(orient="records")
 
